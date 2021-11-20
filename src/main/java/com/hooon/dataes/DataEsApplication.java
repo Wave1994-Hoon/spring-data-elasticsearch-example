2d85f38082fef9eb1b36781950d17488b6bf688a
김광훈 <kk03187693@gmail.com>
2021-11-20 23:24:40 +0900

    initial setting

---
+++ src/main/java/com/hooon/dataes/DataEsApplication.java
@@ -0,0 +1,13 @@
+package com.hooon.dataes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DataEsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hooon/dataes/DataEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooon.dataes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
